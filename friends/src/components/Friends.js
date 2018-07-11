609d1756bb3f6cc9950b22edc58723298a2b8781
bdurb <brian@rpmgreenville.com>
2018-07-10 19:02:58 -0700

    created friends component, set up axios get request inside componentdidmount to recieve data from server

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Friends extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => {
+        this.setState({ friends: res.data})
+      })
+      .catch(error => {
+        console.log("Danger Will Robinson", error);
+      });
   }
   render() { 
     return (
       <div>
-      Hello!
+        {this.state.friends.map(friend => {
+          return <ul>{friend.name}</ul>
+        })}
       </div>
     );
   }
