79287ca3f8c5a6c97044d0e60378076c36000fdb
bdurb <brian@rpmgreenville.com>
2018-07-11 13:38:30 -0700

    fixed some formatting

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -4,12 +4,10 @@ import './Friend.css';
 const Friend = props => {
   return (
     <div className="friendCard">
-      
         Name: {props.friend.name}<br/><br/>
         Age: {props.friend.age}<br/><br/>
         Email: {props.friend.email}
         <button onClick={() => props.handleDeleteFriend(props.friend.id)}>Delete Friend!</button>
-      
     </div>
   );
 }
