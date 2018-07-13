93f69f14e0d6ae0852fc99732569d0c453ef7dda
bdurb <brian@rpmgreenville.com>
2018-07-13 10:02:06 -0700

    refactored put request

--- friends/src/components/EditFriend.js
+++ friends/src/components/EditFriend.js
@@ -3,9 +3,9 @@ import React from 'react';
 const EditFriend = props => {
   return (
     <div>
-      <input placeholder={props.friend.name} type="text" onChange={props.handleTextChange} name="name" value={props.name} />
-      <input placeholder={props.friend.age} type="text" onChange={props.handleTextChange} name="age" value={props.age} />
-      <input placeholder={props.friend.email} type="text" onChange={props.handleTextChange} name="email" value={props.email} />
+      <input placeholder={props.friend.name} type="text" onChange={props.handleTextChange} name="name"  />
+      <input placeholder={props.friend.age} type="text" onChange={props.handleTextChange} name="age"  />
+      <input placeholder={props.friend.email} type="text" onChange={props.handleTextChange} name="email"  />
       <button onClick={props.handleEditFriend}>Confirm Changes</button>
     </div>
   );
